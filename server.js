23d2dd1a0a027a860994c2d26fa915f99f505350
Michel Elmoznino Laufer <mitchel_85@hotmail.com>
2020-08-21 11:53:02 +0200

    Made a minor change

--- server.js
+++ server.js
@@ -71,12 +71,8 @@ const port = process.env.PORT || 8081
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors({origin:'https://michel-charity-finder.netlify.app/'}))
+app.use(cors())
 app.use(bodyParser.json())
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   next()
-// })
 
 
 const authenticateUser = async (req, res, next) => {
